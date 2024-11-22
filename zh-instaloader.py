100e42c15273a4b0d94bf61021a50d986a775d8b
Jhony Arana <jharanza1986@gmail.com>
2024-11-22 01:41:37 -0500

    Add file to run app in Railway

--- zh-instaloader.py
+++ zh-instaloader.py
@@ -53,4 +53,5 @@ def get_latest_reels():
         return jsonify({ 'error': str(e) })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
